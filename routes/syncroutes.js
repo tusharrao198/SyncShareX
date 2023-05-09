e2243dc96017580a29ce2c6c1456e4f7f2bf97c7
Tushar Rao <tushar.rao198@gmail.com>
2023-05-09 10:26:12 +0530

    syncing working from frontend

--- routes/syncroutes.js
+++ routes/syncroutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-// const {} = require("../utils");
+const {readTokenFromOriginalConfig, readTokensFromModifiedConfig, downloadFile, readpendingDL } = require("../utils");
 var url = require("url");
 // const { authCheck } = require("../middleware/auth");
 
@@ -7,8 +7,10 @@ var url = require("url");
 
 router.post("/upload", async (req, res) => {
 	console.log(req.body);
+	const data = await readTokenFromOriginalConfig(req.body);
+	console.log("data return = ", data);
 	res.setHeader("Content-Type", "application/json");
-	res.end(JSON.stringify({ msg: "data" }));
+	res.end(JSON.stringify({ msg: data }));
 });
 
 module.exports = router;
--- utils.js
+++ utils.js
@@ -17,8 +17,167 @@ var errorParsingPendingDL = false;
 
 var downloadRetries = 0;
 
+// readpendingDL function reads the list.txt file and add source folder link into folderList array
+// and destination folder into destinationList.
+
+async function readpendingDL(data) {
+	try {
+		const { localpath, destpath } = data;
+		console.log("Inside readpendingDL  = ", localpath);
+		folderIDList.push(localpath);
+		if (!destpath || destpath == undefined) {
+			destinationList.push("uploads");			
+		} else {
+			destinationList.push(destpath);		
+		}
+
+		// let pendingDLContent = fs
+		// 	.readFileSync(pendingDL, { encoding: "utf-8" })
+		// 	.split(/\r?\n/);
+		// for (let i = 0; i < pendingDLContent.length; i++) {
+		// 	// console.log("Reads List.txt file = ", pendingDLContent);
+		// 	// console.log("\nReads List.txt file\n")
+		// 	if (pendingDLContent[i] != "") {
+		// 		let array = pendingDLContent[i].split("===");
+		// 		if (array.length != 2) {
+		// 			console.log(
+		// 				">>>------ 2. Encountered error while parsing this line:\n>>>------ " +
+		// 					pendingDLContent[i]
+		// 			);
+		// 			errorParsingPendingDL = true;
+		// 		} else {
+		// 			folderIDList.push(array[0]);
+		// 			destinationList.push(array[1]);
+		// 		}
+		// 	}
+		// }
+		console.log("FolderList = ", folderIDList);
+		console.log("destination = ", destinationList);
+	} catch (e) {
+		console.log(
+			">>>------ 3. Encountered error while trying to read the file list:\n>>>------ " +
+				e
+		);
+		errorParsingPendingDL = true;
+	}
+}
+
+async function downloadFile(index) {
+	console.log(
+		">>>------ Copying Folder from " +
+			folderIDList[index] +
+			" to " +
+			destinationList[index]
+	);
+	if (fs.existsSync(modified_config)) fs.unlinkSync(modified_config);
+	fs.copyFileSync(original_config, modified_config);
+	fs.appendFileSync(
+		modified_config,
+		EOL +
+			"[local]" +
+			EOL +
+			"type = local" +
+			EOL
+	);
+	console.log("running rclone now");
+
+	// //create rclone process
+	// const rclone = spawn(
+	// 	rclonePath,
+	// 	[
+	// 		"--drive-server-side-across-configs",
+	// 		"--drive-acknowledge-abuse",
+	// 		"--max-transfer=750G",
+	// 		"--config",
+	// 		modified_config,
+	// 		"-P",
+	// 		"sync",
+	// 		"tmp:",
+	// 		destinationList[index]
+	// 	],
+	// 	{ stdio: 
+	// 		"inherit" }
+	// );
+
+	//create rclone process
+	const rclone = spawn(
+		rclonePath,
+		[
+			"--drive-acknowledge-abuse",
+			"--max-transfer=750G",
+			"--config",
+			modified_config,
+			"-P",
+			"-v",
+			"sync",
+			`local:${folderIDList[index]}`,
+			`teamdrive-jack:${destinationList[index]}`
+			
+		],
+		{ stdio: "inherit" }
+	);
+
+	rclone.on("close", (code) => {
+		readTokensFromModifiedConfig();
+		console.log(">>>------ Child process exited with code " + code);
+		if (code != 0) {
+			if (downloadRetries < retriesBeforeExit) {
+				downloadRetries++;
+				downloadFile(index);
+			} else {
+				console.log(">>>------ Encountered an error.");
+			}
+		} else if (index + 1 == folderIDList.length) {
+			console.log(">>>------ Finished.");
+		} else {
+			downloadRetries = 0;
+			downloadFile(index + 1);
+		}
+	});
+}
+
+async function readTokensFromModifiedConfig() {
+	try {
+		let modifiedConfigContent = fs
+			.readFileSync(modified_config, { encoding: "utf-8" })
+			.split(/\r?\n/);
+		// console.log("modifiedConfigContent = ", modifiedConfigContent);
+		for (let i = 0; i < modifiedConfigContent.length; i++) {
+			// console.log("modifiedConfigContent[i] = ", modifiedConfigContent[i]);
+			if (modifiedConfigContent[i].startsWith("token = ")) {
+				let tmp_token = modifiedConfigContent[i].replace(
+					"token = ",
+					""
+				);
+				let tmp_tokenExpTime = Date.parse(JSON.parse(tmp_token).expiry);
+				
+				// console.log("\n",token,"\n", tmp_token, "\n",tmp_tokenExpTime,"\n", tokenExpTime, "\n");
+
+				if (token != tmp_token && tmp_tokenExpTime > tokenExpTime) {
+					let originalConfigFileContent = fs.readFileSync(
+						original_config,
+						{ encoding: "utf-8" }
+					);
+					originalConfigFileContent =
+						originalConfigFileContent.replace(token, tmp_token);
+					fs.writeFileSync(
+						original_config,
+						originalConfigFileContent
+					);
+					token = tmp_token;
+				}
+			}
+		}
+	} catch (e) {
+		console.log(
+			">>>------ 3. Encountered error trying to read token:\n>>>------ " +
+				e
+		);
+	}
+}
+
 module.exports = {
-	readTokenFromOriginalConfig: async function () {
+	readTokenFromOriginalConfig: async function (data) {
 		try {
 			let configContent = fs
 				.readFileSync(original_config, { encoding: "utf-8" })
@@ -39,159 +198,12 @@ module.exports = {
 		}
 
 		if (token != null) {
-			readpendingDL();
+			readpendingDL(data);
 			if (!errorParsingPendingDL && folderIDList.length != 0) {
 				console.log("B");
 				console.log(">>>------ Start");
 				downloadFile(0);
 			}
 		}
-	},
-
-	// readpendingDL function reads the list.txt file and add source folder link into folderList array
-// and destination folder into destinationList.
-
-	readpendingDL: async function () {
-		try {
-			let pendingDLContent = fs
-				.readFileSync(pendingDL, { encoding: "utf-8" })
-				.split(/\r?\n/);
-			for (let i = 0; i < pendingDLContent.length; i++) {
-				// console.log("Reads List.txt file = ", pendingDLContent);
-				// console.log("\nReads List.txt file\n")
-				if (pendingDLContent[i] != "") {
-					let array = pendingDLContent[i].split("===");
-					if (array.length != 2) {
-						console.log(
-							">>>------ 2. Encountered error while parsing this line:\n>>>------ " +
-								pendingDLContent[i]
-						);
-						errorParsingPendingDL = true;
-					} else {
-						folderIDList.push(array[0]);
-						destinationList.push(array[1]);
-					}
-				}
-			}
-			console.log("FolderList = ", folderIDList);
-			console.log("destination = ", destinationList);
-		} catch (e) {
-			console.log(
-				">>>------ 3. Encountered error while trying to read the file list:\n>>>------ " +
-					e
-			);
-			errorParsingPendingDL = true;
-		}
-	},
-	downloadFile: async function () {
-		console.log(
-			">>>------ Copying Folder from " +
-				folderIDList[index] +
-				" to " +
-				destinationList[index]
-		);
-		if (fs.existsSync(modified_config)) fs.unlinkSync(modified_config);
-		fs.copyFileSync(original_config, modified_config);
-		fs.appendFileSync(
-			modified_config,
-			EOL +
-				"[local]" +
-				EOL +
-				"type = local" +
-				EOL
-		);
-		console.log("running rclone now");
-
-		// //create rclone process
-		// const rclone = spawn(
-		// 	rclonePath,
-		// 	[
-		// 		"--drive-server-side-across-configs",
-		// 		"--drive-acknowledge-abuse",
-		// 		"--max-transfer=750G",
-		// 		"--config",
-		// 		modified_config,
-		// 		"-P",
-		// 		"sync",
-		// 		"tmp:",
-		// 		destinationList[index]
-		// 	],
-		// 	{ stdio: 
-		// 		"inherit" }
-		// );
-
-		//create rclone process
-		const rclone = spawn(
-			rclonePath,
-			[
-				"--drive-acknowledge-abuse",
-				"--max-transfer=750G",
-				"--config",
-				modified_config,
-				"-P",
-				"-v",
-				"sync",
-				`local:${folderIDList[index]}`,
-				destinationList[index]
-			],
-			{ stdio: "inherit" }
-		);
-
-		rclone.on("close", (code) => {
-			readTokensFromModifiedConfig();
-			console.log(">>>------ Child process exited with code " + code);
-			if (code != 0) {
-				if (downloadRetries < retriesBeforeExit) {
-					downloadRetries++;
-					downloadFile(index);
-				} else {
-					console.log(">>>------ Encountered an error.");
-				}
-			} else if (index + 1 == folderIDList.length) {
-				console.log(">>>------ Finished.");
-			} else {
-				downloadRetries = 0;
-				downloadFile(index + 1);
-			}
-		});
-	},
-	readTokensFromModifiedConfig: async function () {
-		try {
-				let modifiedConfigContent = fs
-					.readFileSync(modified_config, { encoding: "utf-8" })
-					.split(/\r?\n/);
-				// console.log("modifiedConfigContent = ", modifiedConfigContent);
-				for (let i = 0; i < modifiedConfigContent.length; i++) {
-					// console.log("modifiedConfigContent[i] = ", modifiedConfigContent[i]);
-					if (modifiedConfigContent[i].startsWith("token = ")) {
-						let tmp_token = modifiedConfigContent[i].replace(
-							"token = ",
-							""
-						);
-						let tmp_tokenExpTime = Date.parse(JSON.parse(tmp_token).expiry);
-						
-						// console.log("\n",token,"\n", tmp_token, "\n",tmp_tokenExpTime,"\n", tokenExpTime, "\n");
-
-						if (token != tmp_token && tmp_tokenExpTime > tokenExpTime) {
-							let originalConfigFileContent = fs.readFileSync(
-								original_config,
-								{ encoding: "utf-8" }
-							);
-							originalConfigFileContent =
-								originalConfigFileContent.replace(token, tmp_token);
-							fs.writeFileSync(
-								original_config,
-								originalConfigFileContent
-							);
-							token = tmp_token;
-						}
-					}
-				}
-			} catch (e) {
-				console.log(
-					">>>------ 3. Encountered error trying to read token:\n>>>------ " +
-						e
-				);
-			}
 	},
 };
